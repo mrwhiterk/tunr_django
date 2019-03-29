998bf1f1aaca9166f9f91b355cb6e607e33122d1
Ryan White <mrwhiterk@gmail.com>
2019-03-29 16:08:20 -0400

    got artist and song show working

--- tunr/urls.py
+++ tunr/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.artist_list, name='artist_list')
+    path('', views.artist_list, name='artist_list'),
+    path('artists/<int:pk>', views.artist_show, name='artist_show'),
+    path('songs/', views.song_list, name='song_list'),
+    path('songs/<int:pk>', views.song_detail, name='song_detail')
 ]
