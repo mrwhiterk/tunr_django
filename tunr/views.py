7ec7a03657130f649962f6ad1817a26933ae71ca
Ryan White <mrwhiterk@gmail.com>
2019-03-29 15:07:28 -0400

    working on views and templates

--- tunr/views.py
+++ tunr/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Artist, Song
+
+
+def artist_list(request):
+    artists = Artist.objects.all()
+    return render(request, 'tunr/artist_list.html', {'artists': artists})
